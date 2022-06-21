e76330090d3cf4c6276a22e0d0753f0569caebec
MaximilianoPola <maxipola95@gmail.com>
2022-06-21 20:06:06 -0300

    micro desafios  terminados

--- app.js
+++ app.js
@@ -12,8 +12,17 @@ app.use(express.static(publicPath))
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname , "./views/home.html"));
 });
+
 app.get("/home", function(req, res){
     res.sendFile(path.resolve(__dirname , "./views/home.html"));
+});
+
+app.get("/register", function(req, res){
+    res.sendFile(path.resolve(__dirname , "./views/register.html"));
+});
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname , "./views/login.html"));
 });
 
 
